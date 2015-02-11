f2049f55698493f6383796b32af516de67cc34a2
Weidan <idoudou92@gmail.com>
2015-02-11 13:52:45 -0500

    hw2start

---
+++ Script_HW2.js
@@ -0,0 +1,17 @@
+
+    $(document).ready(function () {
+       $.getJSON("https://hivelab.org/static/students.json", function (json) {
+        var tr;
+        for (var i = 0; i < json.length; i++) {
+           tr = $('<tr/>');
+            tr.append("<td>" + json[i].Name + "</td>");
+            tr.append("<td>" + json[i].GPA + "</td>");
+            tr.append("<td>" + json[i].GRE_V + "</td>");
+            tr.append("<td>" + json[i].GRE_Q + "</td>");
+            tr.append("<td>" + json[i].Essay+ "</td>");
+            tr.append("<td>" + json[i].Recom+ "</td>");
+            $('#hw2table').append(tr); 
+        }
+   
+		});
+	});// JavaScript Document*/
